ddf10036c3a5781fb29d8c3114a0f0e2c3f2dacd
Azizul Islam <contact.azizulislam@gmail.com>
2025-03-12 09:05:23 +0600

    Home Screen Added

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:projects/app/assets_path.dart';
+import 'package:projects/features/home/ui/widgets/app_bar_icon_button.dart';
+import 'package:projects/features/home/ui/widgets/product_search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,13 +15,60 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController _searchBarController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Home'),
+      appBar: _buildAppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              ProductSearchBar(controller: _searchBarController),
+              const SizedBox(height: 16),
+              CarouselSlider(
+                options: CarouselOptions(height: 200),
+                items:
+                    [1, 2, 3, 4, 5].map((i) {
+                      return Builder(
+                        builder: (BuildContext context) {
+                          return Container(
+                            width: MediaQuery.of(context).size.width,
+                            margin: EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+
+                            child: Text(
+                              'text $i',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          );
+                        },
+                      );
+                    }).toList(),
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetsPath.navBarAppLogoSvg),
+      actions: [
+        AppBarIconButton(icon: Icons.person_outline, onTap: () {}),
+        const SizedBox(width: 6),
+        AppBarIconButton(icon: Icons.call, onTap: () {}),
+        const SizedBox(width: 6),
+        AppBarIconButton(icon: Icons.notifications_on_outlined, onTap: () {}),
+      ],
     );
   }
 }
---
+++ lib/features/home/ui/widgets/product_search_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProductSearchBar extends StatelessWidget {
+  const ProductSearchBar({
+    super.key, required this.controller,
+  });
+
+  final TextEditingController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+        border: OutlineInputBorder(borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide.none,
+        ),
+        fillColor: Colors.grey.shade100,
+        filled: true,
+        hintText: 'Search',
+        prefixIcon: Icon(Icons.search, color: Colors.grey),
+      ),
+    );
+  }
+}
