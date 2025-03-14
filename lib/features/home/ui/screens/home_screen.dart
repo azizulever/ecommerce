c281134f6d74c7c6dac4e34603375ee935b6d236
Azizul Islam <contact.azizulislam@gmail.com>
2025-03-14 10:00:57 +0600

    Carausol Edited

--- lib/features/home/ui/screens/home_screen.dart
+++ lib/features/home/ui/screens/home_screen.dart
@@ -1,8 +1,8 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:projects/app/assets_path.dart';
 import 'package:projects/features/home/ui/widgets/app_bar_icon_button.dart';
+import 'package:projects/features/home/ui/widgets/home_carousel_slider.dart';
 import 'package:projects/features/home/ui/widgets/product_search_bar.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -29,28 +29,7 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 16),
               ProductSearchBar(controller: _searchBarController),
               const SizedBox(height: 16),
-              CarouselSlider(
-                options: CarouselOptions(height: 200),
-                items:
-                    [1, 2, 3, 4, 5].map((i) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                              color: Colors.amber,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              'text $i',
-                              style: TextStyle(fontSize: 16.0), textAlign: TextAlign.center,
-                            ),
-                          );
-                        },
-                      );
-                    }).toList(),
-              ),
+              HomeCarouselSlider(),
             ],
           ),
         ),
