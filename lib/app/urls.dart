4f1c9ec6d5a384824f81a4e68f6c8ee66febab46
Azizul Islam <contact.azizulislam@gmail.com>
2025-04-24 23:00:24 +0600

    otp verification

--- lib/app/urls.dart
+++ lib/app/urls.dart
@@ -2,5 +2,5 @@ class Urls {
   static const String _baseUrl = 'https://ecom-rs8e.onrender.com/api';
   static String verifyEmailUrl = '$_baseUrl/auth/login';
   static String verifyOtpUrl(String email, String otp) =>
-      '$_baseUrl/auth/verify-otp';
+      '$_baseUrl/auth/verify-otp/$email/$otp';
 }
